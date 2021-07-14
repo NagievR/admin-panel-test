45e20039265109c90a638b0cb1aefbe00d748229
Ruslan Nagiev <ultraduck100@gmail.com>
2021-07-14 18:02:58 +0300

    add db

---
+++ database/mongoose-db.js
@@ -0,0 +1,17 @@
+const connectToDB = () => {
+  const mongoose = require('mongoose');
+
+  mongoose.connect(
+    "mongodb+srv://nagievr:1234qwer@cluster0.lojxe.mongodb.net/admin-test?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+
+  const connection = mongoose.connection;
+  
+  connection.on('error', () => console.error('DB connection error'));
+  connection.on('open', () => console.log('successful connection to DB'));
+}
+
+module.exports = connectToDB;
--- routes/admin.js
+++ routes/admin.js
@@ -1,30 +1,9 @@
-// const AdminBro = require('admin-bro');
-// const AdminBroExpress = require('@admin-bro/express');
-
-// const adminBro = new AdminBro({
-//   databases: [],
-//   rootPath: '/admin',
-// });
-
-// const router = AdminBroExpress.buildRouter(adminBro);
-
-// // app.use(adminBro.options.rootPath, router);
-// // app.listen(8080, () => console.log('AdminBro is under localhost:8080/admin'));
-
-// module.exports = router;
-
-
 const AdminBro = require('admin-bro')
 const AdminBroExpress = require('@admin-bro/express')
 
-console.log(AdminBro, AdminBroExpress);
-
-// const express = require('express')
-// const app = express()
-
 const adminBro = new AdminBro({
   databases: [],
-  rootPath: '/admin',
+  // rootPath: '/admin',
 })
 
 const router = AdminBroExpress.buildRouter(adminBro)
